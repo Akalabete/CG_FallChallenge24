86ce798522c1793e34420f9e0827bef6a1854314
LACOUR Alexandre <lacour_alexandre@proton.me>
2025-01-23 17:11:29 +0100

    debug in progress

--- game.js
+++ game.js
@@ -67,76 +67,75 @@ class City {
         // update active pod list
         this.podlist = this.podList.filter(pod => pod.isActive === true)
     }       
-    updateNewBuildings(numNewBuildings, buildingProperties) {
-        for (let i = 0; i < numNewBuildings; i++) {
-            // extract properties
-            let BPArray = buildingProperties.split(' ');
-            // verify if it is a landing area or a building
-            if (BPArray[0] === 0) {
-                if(BPArray[4] > 0){
-                    let astroType1 = 0;
-                    let astroType2 = 0;
-                    let astroType3 = 0;
-                    let astroType4 = 0;
-                    let astroType5 = 0;
-                    let astroType6 = 0;
-                    // create and populate an array of astronauts types
-                    for(let j = 5; j<BPArray.length; j++) {
-                        switch (BPArray[j]) {
-                            case 1:
-                                astroType1++;
-                                break;
-                            case 2:
-                                astroType2++;
-                                break;
-                            case 3:
-                                astroType3++;
-                                break;
-                            case 4:
-                                astroType4++;
-                                break;
-                            case 5:
-                                astroType5++;
-                                break;
-                            case 6:
-                                astroType6++;
-                                break;
-                            default:
-                                break;
-                        }
+    updateNewBuildings(buildingProperties) {
+        // extract properties
+        let BPArray = buildingProperties.split(' ')
+        // verify if it is a landing area or a building
+        if (BPArray[0] === '0') {
+            if(BPArray[4] > 0){
+                let astroType1 = 0;
+                let astroType2 = 0;
+                let astroType3 = 0;
+                let astroType4 = 0;
+                let astroType5 = 0;
+                let astroType6 = 0;
+                // create and populate an array of astronauts types
+                for(let j = 5; j<BPArray.length; j++) {
+                    switch (BPArray[j]) {
+                        case 1:
+                            astroType1++;
+                            break;
+                        case 2:
+                            astroType2++;
+                            break;
+                        case 3:
+                            astroType3++;
+                            break;
+                        case 4:
+                            astroType4++;
+                            break;
+                        case 5:
+                            astroType5++;
+                            break;
+                        case 6:
+                            astroType6++;
+                            break;
+                        default:
+                            break;
                     }
-                    // create and push the object in the buildings array
-                    const landingArea = new LandingArea(
-                        BPArray[1],
-                        "landing area",
-                        BPArray[2],
-                        BPArray[3],
-                        BPArray[4],
-                        [astroType1, astroType2, astroType3, astroType4, astroType5, astroType6],
-                        0,
-                        false,
-                    );
-                    this.buildings.push(landingArea);
-                } else {
-                    return;
                 }
-                // create and push the lunar module object in the buildings array
-            }else {
-                const building= new Building(
+                // create and push the object in the buildings array
+                const landingArea = new LandingArea(
                     BPArray[1],
-                    "lunar module",
-                    BPArray[0],
+                    "landing area",
                     BPArray[2],
                     BPArray[3],
+                    BPArray[4],
+                    [astroType1, astroType2, astroType3, astroType4, astroType5, astroType6],
                     0,
                     false,
-                    false,
                 );
-                this.buildings.push(building);
+                this.buildings.push(landingArea);
+            } else {
+                return;
             }
+            // create and push the lunar module object in the buildings array
+        }else {
+            const building= new Building(
+                BPArray[1],
+                "lunar module",
+                BPArray[0],
+                BPArray[2],
+                BPArray[3],
+                0,
+                false,
+                false,
+            );
+            this.buildings.push(building);
         }
     }
 }
+
 // fn that calculate the length of the travel route
 function calculateLength(building1ID, building2ID) {
     const building1 = this.buildings.find(building => building.id === building1ID);
@@ -148,26 +147,31 @@ function pointOnSegment(A, B, C) {
     epsilon = 0.0000001
     return (-epsilon < distance(B, A) + distance(A, C) - distance(B, C) < epsilon)
 }
-// function that find the closests buildings from 1 and give an array
+// fn that find the closests buildings from 1 and give an array
 function findClosestBuildingIds(buildingA) {
-    const distances = city.buildings.map((building) => {
+    // filter buildingA from the list
+    const cityBuildingListMinusA = city.buildings.filter(building => building.id !== buildingA.id);
+
+    // calculate distance for buildings
+    const distances = cityBuildingListMinusA.map((building) => {
         const distance = Math.sqrt(Math.pow(buildingA.x - building.x, 2) + Math.pow(buildingA.y - building.y, 2));
         return { id: building.id, distance: distance };
     });
 
+    // sort the distances
     distances.sort((a, b) => a.distance - b.distance);
 
-    return distances.map((building) => building.id);
+    // return id & distance in an array
+    return distances;
 }
 // fn that filters the building without connection
 function buildingFiltering() {
-    // checking if building are linked
-    toLinkBuildings = city.buildings.filter(buildings => buildings.hasTR === 0 && buildings.hasTP === false)
+    // checking if buildings are linked
     // list of unlinked landing areas
-    unlinkedLA = toLinkBuildings.find(building => building.type === 'landing area')
+    const unlinkedLA = city.buildings.find(building => building.type === 'landing area' && building.monthlyArrivals > 0 && building.hasTR === 0);
     // list of unlinked lunar modules
-    unlinkedLM = toLinkBuildings.find(building => building.type === 'lunar module')
-    return toLinkBuildings, unlinkedLA, unlinkedLM;
+    const unlinkedLM = city.buildings.find(building => building.type === 'lunar module' && building.hasTR === 0);
+    return {unlinkedLA, unlinkedLM };
 }
 // describe lunar module building model
 class Building {
@@ -247,35 +251,38 @@ while (true) {
     const numNewBuildings = parseInt(readline());
     for (let i = 0; i < numNewBuildings; i++) {
         const buildingProperties = readline();
-        city.updateNewBuildings(numNewBuildings, buildingProperties);
+        city.updateNewBuildings(buildingProperties);
     }
     let action = 'WAIT';
     // Write an action using console.log()
     // To debug: console.error('Debug messages...');
     // TUBE | UPGRADE | TELEPORT | POD | DESTROY | WAIT
     // PRIORITY 1 : LINK BUILDINGS
-    buildingFiltering();
-    while(toLinkBuildings.length > 0 && city.ressources > 500){
+   
+    const { unlinkedLA, unlinkedLM } = buildingFiltering();  
+    if (unlinkedLA && city.ressources > 500) {
         action = "";
-        if(unlinkedLA) {
-            closestNodes = findClosestBuildingId(unlinkedLA)
-            const tolerance = 0.01;
-            const referenceDistance = closestNodes[0].distance;
-            const filteredNodes = closestNodes.filter((node) => {
-                return Math.abs(node.distance - referenceDistance) <= tolerance;
-            });
-            filteredNodes.forEach((node) => {
-                if (city.ressources > 500){
-                    action += `TUBE ${unlinkedLA.id} ${node.id};`;
-                    city.buildings[unlinkedLA.id].hasTR++;
-                    city.buildings[node.id].hasTR++;
-                    city.ressources -= 500;
-                }else {
-                    return;
-                }
-            });
-        }
+        const closestNodes = findClosestBuildingIds(unlinkedLA);
+        console.error(closestNodes);
+        const tolerance = 0.01;
+        const referenceDistance = closestNodes[0].distance;
+        const filteredNodes = closestNodes.filter((node) => {
+            return Math.abs(node.distance - referenceDistance) <= tolerance;
+        });
+        filteredNodes.forEach((node) => {
+            if (city.ressources > 500){
+                action += `TUBE ${unlinkedLA.id} ${node.id};`;
+                city.buildings[unlinkedLA.id].hasTR++;
+                city.buildings[node.id].hasTR++;
+                city.ressources -= 500;
+            } else {
+                return;
+            }
+        });
     }
+        
+    
+    
     // PRIORITY II :  Pods
     
     // spend vs save ressources
